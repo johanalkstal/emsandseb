a4a693be479b7a99b60e5a99f212e53c4255a823
Johan Alkstål <hi@johanalkstal.com>
2020-01-11 11:15:32 +0100

    Update configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Emelie och Sebastian',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Emelie och Sebastian ska gifta sig',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
